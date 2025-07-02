6e08190e984a02ff71d164e6ec7736e89481b3b8
Daisuke Nashimoto <Daisuke.Nashimoto@jtp.co.jp>
2025-07-02 19:07:16 +0900

    APIのエンドポイントを変更

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,8 +9,8 @@ const nextConfig = {
       {
         source: '/api/:path*',
         destination: process.env.NEXT_PUBLIC_API_URL 
-          ? `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`
-          : 'http://localhost:8000/api/:path*',
+          ? `${process.env.NEXT_PUBLIC_API_URL}/:path*`
+          : 'http://localhost:8000/v1/:path*',
       },
     ];
   },
